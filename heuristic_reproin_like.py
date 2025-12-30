4e60d643287b68cb7912cc399b194a5f986b70c3
XiaoqianXiao <xiao.xiaoqian.320@gmail.com>
2025-12-30 15:35:35 -0800

    add sbatch scripts

--- heuristic_reproin_like.py
+++ heuristic_reproin_like.py
@@ -5,57 +5,32 @@ def create_key(template, outtype=('nii.gz',), annotation_classes=None):
         raise ValueError('Template must be a valid format string')
     return (template, outtype, annotation_classes)
 
-# UPDATE: Added subject and session to arguments to allow sanitization
-def infotodict(seqinfo, subject=None, session=None):
-    """
-    Heuristic to parse ReproIn-style protocol names directly.
-    """
-    
-    # --- SANITIZATION STEP ---
-    # This ensures we don't get 'ses-ses-pilot' even if the bash script passes 'ses-pilot'
-    clean_session = str(session)
-    if clean_session.startswith('ses-'):
-        clean_session = clean_session[4:]
-    
-    # We hardcode the cleaned session into the string below, 
-    # instead of letting HeuDiConv interpolate {session} automatically.
-    # -------------------------
-
+def infotodict(seqinfo):
     info = {}
-    
     # BIDS entities order
     bids_order = ['task', 'acq', 'ce', 'rec', 'dir', 'run', 'echo', 'part']
 
     for s in seqinfo:
         pname = s.protocol_name
-        
         if not pname or '-' not in pname:
             continue
 
         parts = pname.split('_')
-        
-        # 1. Parse Datatype and Suffix
         head = parts[0].split('-')
-        if len(head) < 2:
-            continue 
+        if len(head) < 2: continue 
             
-        datatype = head[0] # e.g., func
-        suffix = head[1]   # e.g., bold
+        datatype = head[0] # func
+        suffix = head[1]   # bold
         
-        # 2. Parse Entities
         entities = {}
         for part in parts[1:]:
             if '-' in part:
                 key, value = part.split('-', 1)
                 entities[key] = value
         
-        # 3. Construct the BIDS filename
-        # Note: We use {subject} (letting HeuDiConv fill it), 
-        # but we use our sanitized f'{clean_session}' immediately.
-        filename_parts = [
-            'sub-{subject}', 
-            f'ses-{clean_session}'  # <--- This guarantees exactly one prefix
-        ]
+        # NOTE: We use 'ses-{session}' here. 
+        # The Bash script MUST pass a clean ID (e.g., '01') for this to work.
+        filename_parts = ['sub-{subject}', 'ses-{session}']
         
         for entity_key in bids_order:
             if entity_key in entities:
@@ -65,14 +40,10 @@ def infotodict(seqinfo, subject=None, session=None):
         filename_parts.append(suffix)
         filename_base = '_'.join(filename_parts)
         
-        # Path: sub-{subject}/ses-{clean_session}/{datatype}/{filename_base}
-        out_template = f'sub-{{subject}}/ses-{clean_session}/{datatype}/{filename_base}'
+        out_template = f'sub-{{subject}}/ses-{{session}}/{datatype}/{filename_base}'
         
         key = create_key(out_template)
-        
-        if key not in info:
-            info[key] = []
-            
+        if key not in info: info[key] = []
         info[key].append(s.series_id)
 
     return info
