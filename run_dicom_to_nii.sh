738f2309d9cdb902d775332a006281fcb89ceb7a
XiaoqianXiao <xiao.xiaoqian.320@gmail.com>
2025-12-30 11:47:41 -0800

    update the dicom to nii script

--- run_dicom_to_nii.sh
+++ run_dicom_to_nii.sh
@@ -46,6 +46,7 @@ for SUBJ_PATH in "${DICOM_INPUT}"/*; do
   singularity exec \
     -B "${DICOM_INPUT}:/dicom:ro" \
     -B "${BIDS_OUTPUT}:/bids" \
+    -B ${HEURISTIC}:/HEURISTIC \
     "${HEUDICONV_SIF}" \
     heudiconv \
       -d /dicom/{subject}/{session}/*/*/*.dcm \
