38ba1214ad727d25425b9e84e57b73dcac1951d6
XiaoqianXiao <xiao.xiaoqian.320@gmail.com>
2025-12-30 12:47:57 -0800

    update the dicom to nii script

--- heuristic_reproin_like.py
+++ heuristic_reproin_like.py
@@ -1,38 +1,81 @@
-def create_key(template):
-    return (template, ('nii.gz',), None)
+import os
 
-t1w = create_key(
-    'sub-{subject}/ses-{session}/anat/sub-{subject}_ses-{session}_T1w'
-)
-
-bold = create_key(
-    'sub-{subject}/ses-{session}/func/sub-{subject}_ses-{session}_task-{task}_run-{run}_bold'
-)
-
-fmap = create_key(
-    'sub-{subject}/ses-{session}/fmap/sub-{subject}_ses-{session}_dir-{dir}_epi'
-)
+def create_key(template, outtype=('nii.gz',), annotation_classes=None):
+    if template is None or not template:
+        raise ValueError('Template must be a valid format string')
+    return (template, outtype, annotation_classes)
 
 def infotodict(seqinfo):
-    info = {
-        t1w: [],
-        bold: [],
-        fmap: [],
-    }
+    """
+    Heuristic to parse ReproIn-style protocol names directly.
+    Expects protocol_names (folder names) like:
+    - func-bold_task-rest_run-01
+    - anat-T1w
+    - fmap-epi_dir-AP
+    - dwi-dwi_acq-98dir_dir-AP
+    """
+    
+    info = {}
+    
+    # BIDS entities order is strict. 
+    # We will parse the folder name parts and re-assemble them in this order.
+    # Note: 'mod' and 'recording' are less common but included for completeness.
+    bids_order = ['task', 'acq', 'ce', 'rec', 'dir', 'run', 'echo', 'part']
 
     for s in seqinfo:
-        name = (s.protocol_name or '').lower()
+        # ReproIn convention: The protocol name usually acts as the source
+        # format: datatype-suffix_entity-value_entity-value...
+        pname = s.protocol_name
+        
+        # Skip if no protocol name or likely not a ReproIn sequence
+        if not pname or '-' not in pname:
+            continue
 
-        # T1w
-        if 't1w' in name:
-            info[t1w].append(s.series_id)
-
-        # BOLD
-        elif 'bold' in name:
-            info[bold].append(s.series_id)
-
-        # Fieldmaps
-        elif 'fmap' in name or 'epi' in name:
-            info[fmap].append(s.series_id)
+        parts = pname.split('_')
+        
+        # 1. Parse Datatype and Suffix (First segment: e.g., "func-bold")
+        head = parts[0].split('-')
+        if len(head) < 2:
+            continue # malformed
+            
+        datatype = head[0] # e.g., func, anat, dwi, fmap
+        suffix = head[1]   # e.g., bold, T1w, dwi, epi
+        
+        # 2. Parse Entities (Remaining segments: e.g., "task-rest", "run-01")
+        entities = {}
+        for part in parts[1:]:
+            if '-' in part:
+                key, value = part.split('-', 1)
+                entities[key] = value
+        
+        # 3. Construct the BIDS filename
+        # Start with subject and session
+        # pattern: sub-{subject}_ses-{session}
+        filename_parts = ['sub-{subject}', 'ses-{session}']
+        
+        # Add entities in strict BIDS order
+        for entity_key in bids_order:
+            if entity_key in entities:
+                # Handle special casing if needed (usually just string insertion)
+                val = entities[entity_key]
+                filename_parts.append(f'{entity_key}-{val}')
+        
+        # Add suffix
+        filename_parts.append(suffix)
+        
+        # Join with underscores
+        filename_base = '_'.join(filename_parts)
+        
+        # Construct full path key
+        # Path: sub-{subject}/ses-{session}/{datatype}/{filename_base}
+        out_template = f'sub-{{subject}}/ses-{{session}}/{datatype}/{filename_base}'
+        
+        # 4. Create the key and assign series
+        key = create_key(out_template)
+        
+        if key not in info:
+            info[key] = []
+            
+        info[key].append(s.series_id)
 
     return info
