cbdd97e7df6bcdc7d7edb0928221a6e871bd8608
XiaoqianXiao <xiao.xiaoqian.320@gmail.com>
2025-12-30 15:42:08 -0800

    add sbatch scripts

--- heuristic_reproin_like.py
+++ heuristic_reproin_like.py
@@ -5,32 +5,53 @@ def create_key(template, outtype=('nii.gz',), annotation_classes=None):
         raise ValueError('Template must be a valid format string')
     return (template, outtype, annotation_classes)
 
-def infotodict(seqinfo):
+# We ask HeuDiConv to pass us the 'subject' and 'session' arguments
+def infotodict(seqinfo, subject=None, session=None):
+    
+    # --- SMART SESSION HANDLING ---
+    # This logic prevents "ses-ses-" duplication no matter what the bash script sends.
+    if session and str(session).startswith('ses-'):
+        # Input is 'ses-pilot', so we just use '{session}'
+        ses_fmt = "{session}"
+    else:
+        # Input is 'pilot', so we must add 'ses-{session}'
+        ses_fmt = "ses-{session}"
+    # ------------------------------
+
     info = {}
+    
     # BIDS entities order
     bids_order = ['task', 'acq', 'ce', 'rec', 'dir', 'run', 'echo', 'part']
 
     for s in seqinfo:
         pname = s.protocol_name
+        
         if not pname or '-' not in pname:
             continue
 
         parts = pname.split('_')
-        head = parts[0].split('-')
-        if len(head) < 2: continue 
-            
-        datatype = head[0] # func
-        suffix = head[1]   # bold
         
+        # 1. Parse Datatype and Suffix
+        head = parts[0].split('-')
+        if len(head) < 2:
+            continue 
+            
+        datatype = head[0] # e.g., func
+        suffix = head[1]   # e.g., bold
+        
+        # 2. Parse Entities
         entities = {}
         for part in parts[1:]:
             if '-' in part:
                 key, value = part.split('-', 1)
                 entities[key] = value
         
-        # NOTE: We use 'ses-{session}' here. 
-        # The Bash script MUST pass a clean ID (e.g., '01') for this to work.
-        filename_parts = ['sub-{subject}', '{session}']
+        # 3. Construct the BIDS filename
+        # We use our smart 'ses_fmt' variable here
+        filename_parts = [
+            'sub-{subject}', 
+            ses_fmt
+        ]
         
         for entity_key in bids_order:
             if entity_key in entities:
@@ -40,10 +61,14 @@ def infotodict(seqinfo):
         filename_parts.append(suffix)
         filename_base = '_'.join(filename_parts)
         
-        out_template = f'sub-{{subject}}/{{session}}/{datatype}/{filename_base}'
+        # Construct full path
+        out_template = f'sub-{{subject}}/{ses_fmt}/{datatype}/{filename_base}'
         
         key = create_key(out_template)
-        if key not in info: info[key] = []
+        
+        if key not in info:
+            info[key] = []
+            
         info[key].append(s.series_id)
 
     return info
