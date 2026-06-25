d3e45bf7be0d2e7a4579f6c3cf22c78119328610
XiaoqianXiao <xiao.xiaoqian.320@gmail.com>
2026-06-25 10:11:36 -0700

    add byTime script

---
+++ download_bids_subjects_on_hyak_byTime.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+set -euo pipefail
+
+IMAGE="/gscratch/fang/images/flywheel.sif"
+BIND_SRC="/gscratch/fang/IFOCUS/sourcedata/MRI"
+BIND_DEST="/DATA_DIR"
+PROJECT_PATH="fang-lab/IFOCUS"
+
+apptainer exec --env FW_KEY="$FW_KEY" -B "${BIND_SRC}:${BIND_DEST}" "$IMAGE" sh -c \
+     "fw login \"\$FW_KEY\" && fw download $PROJECT_PATH -o ${BIND_DEST} --include dicom"
