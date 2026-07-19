9dee6b112f2ef2daa904cf09b925c4ef743b0111
XiaoqianXiao <xiao.xiaoqian.320@gmail.com>
2026-07-19 16:50:40 -0700

    update scripts for data after 20260622

--- download_bids_subjects_on_hyak_byTime.sh
+++ download_bids_subjects_on_hyak_byTime.sh
@@ -7,4 +7,5 @@ BIND_DEST="/DATA_DIR"
 PROJECT_PATH="fang-lab/IFOCUS"
 
 apptainer exec --env FW_KEY="$FW_KEY" -B "${BIND_SRC}:${BIND_DEST}" "$IMAGE" sh -c \
-     "fw login \"\$FW_KEY\" && fw sync $PROJECT_PATH ${BIND_DEST} --include 'session.timestamp > 2026-01-01' --include dicom --jobs 4"
+     "fw login \"\$FW_KEY\" && fw sync $PROJECT_PATH ${BIND_DEST} --include 'session.timestamp > 2026-06-22' --include dicom --jobs 4"
+     #"fw login \"\$FW_KEY\" && fw sync $PROJECT_PATH ${BIND_DEST} --include 'session.timestamp > 2026-01-01' --include dicom --jobs 4"
